93d8ed81e3ac07982b5eee3c385e49d621477593
Spindler, Sören <SpindlerS@rki.de>
2018-01-11 19:51:33 +0100

    Adds FsUnit to test project. Adds first unit test for initial game state (not completed).

--- Chess/Chess.Core/Library.fs
+++ Chess/Chess.Core/Library.fs
@@ -12,11 +12,25 @@ module BasicTypes =
             Column: int;
         }
          
+    // Die Figuren haben alle eine Farbe und eine Position auf dem Brett.
+    // Wurde eine Figur geschlagen, hat sie keine Position mehr auf dem Brett -> deshalb option.
+    // ABER: wird der König geschlagen bzw. hat keine Zugmöglichkeit mehr, ist das Spiel beendet.
     type ChessPiece = 
-        | Pawn of Color * Position      // Bauer
-        | Knight of Color * Position    // Springer
-        | Bishop of Color * Position    // Läufer
-        | Rook of Color * Position      // Turm
-        | Queen of Color * Position     // Dame
-        | King of Color * Position      // König
+        | Pawn of Color * Position option      // Bauer
+        | Knight of Color * Position option    // Springer
+        | Bishop of Color * Position option    // Läufer
+        | Rook of Color * Position option      // Turm
+        | Queen of Color * Position option     // Dame
+        | King of Color * Position             // König
+
+    type GameState = ChessPiece list  
+
+module BasicFunctions =
+    
+    open BasicTypes
+    
+    let createInitialGameState: GameState =
         
+        list.Empty
+
+
